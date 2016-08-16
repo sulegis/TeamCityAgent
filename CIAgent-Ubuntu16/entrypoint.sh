e323e251d692476e6cf9e54597151d7489e38d7f
sulele <sulele@supermap.com>
2016-08-16 14:01:58 +0800

    Ubuntu 16.04 with lib

---
+++ CIAgent-Ubuntu16/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -z "$TEAMCITY_SERVER" ]; then
+    echo "TEAMCITY_SERVER variable not set, launch with -e TEAMCITY_SERVER=http://mybuildserver"
+    exit 1
+fi
+
+if [ -z "$TEAMCITY_NAME" ]; then
+    echo "TEAMCITY_NAME variable not set, launch with -e TEAMCITY_NAME=NAME"
+    exit 1
+fi
+
+if [ ! -d "$AGENT_DIR/bin" ]; then
+    echo "$AGENT_DIR doesn't exist pulling build-agent from server $TEAMCITY_SERVER";
+    let waiting=0
+    until curl -s -f -I -X GET $TEAMCITY_SERVER/update/buildAgent.zip; do
+        let waiting+=3
+        sleep 3
+        if [ $waiting -eq 120 ]; then
+            echo "Teamcity server did not respond within 120 seconds"...
+            exit 42
+        fi
+    done
+    wget $TEAMCITY_SERVER/update/buildAgent.zip && unzip -d $AGENT_DIR buildAgent.zip && rm buildAgent.zip
+    chmod +x $AGENT_DIR/bin/agent.sh
+    echo "serverUrl=${TEAMCITY_SERVER}" >> $AGENT_DIR/conf/buildAgent.properties
+	echo "name=${TEAMCITY_NAME}" >> $AGENT_DIR/conf/buildAgent.properties
+	
+fi
+
+echo "Starting buildagent..."
+/opt/buildAgent/bin/agent.sh run
